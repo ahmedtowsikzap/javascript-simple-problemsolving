dec3ecaa61598d05db1b09d39d4964c8f496f816
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-17 20:14:42 +0600

    Local Storage stuff

---
+++ localstorage.js
@@ -0,0 +1,12 @@
+const addtoLocalStorage = () => {
+
+    const idInput = document.getElementById('storage-id')
+    const id = idInput.value
+    const idValue = document.getElementById('storage-value')
+    const value = idValue.value;
+
+    //
+    localStorage.setItem(id, value);
+    idInput.value = '';
+    idValue.value = '';
+}
